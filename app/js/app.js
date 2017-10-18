f1fdf2236d8a1cf53bb49fac3cceadcb290fdafe
Valentina <esetnod1111@gmail.com>
2017-10-18 09:32:46 +0300

    third

--- app/js/app.js
+++ app/js/app.js
@@ -7,3 +7,11 @@ function expandfullscreenmenu(action){ //param: 'open', 'close', or empty to tog
   var newstate = (action == 'open')? true : (action == 'close')? false : !togglebox.checked
   togglebox.checked = newstate
 }
+
+jQuery(function($){
+   $("#phone1").mask("9(999) 999-9999");
+$("#phone2").mask("9(999) 999-9999");
+$("#phone3").mask("9(999) 999-9999");
+$("#phone4").mask("9(999) 999-9999");
+$("#phone5").mask("9(999) 999-9999");
+   });
